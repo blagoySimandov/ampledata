9f7fe63393f862afe9a0503ce3d0340bbc842217
blagoySimandov <baggy.simandoff@gmail.com>
2025-12-25 22:49:49 +0200

    feat: run_config added to web_crawler

--- python/ampledata/web_crawler.py
+++ python/ampledata/web_crawler.py
@@ -1,4 +1,6 @@
-from crawl4ai import AsyncWebCrawler
+from crawl4ai import AsyncWebCrawler, CrawlerRunConfig, CacheMode, types
+from crawl4ai.content_filter_strategy import PruningContentFilter
+from crawl4ai.markdown_generation_strategy import DefaultMarkdownGenerator
 from abc import abstractmethod, ABC
 
 
@@ -13,8 +15,19 @@ class IWebCrawler(ABC):
 
 
 class WebCrawler(IWebCrawler):
+    run_config = CrawlerRunConfig(
+        cache_mode=CacheMode.ENABLED,
+        markdown_generator=DefaultMarkdownGenerator(
+            content_filter=PruningContentFilter(threshold=0.48)
+        ),
+        word_count_threshold=10,
+        excluded_tags=["form", "header", "footer", "nav"],
+        magic=True,
+        simulate_user=True,
+    )
+
     def __init__(self):
-        self.crawler: AsyncWebCrawler = AsyncWebCrawler()
+        self.crawler = AsyncWebCrawler()
         self._started = False
 
     async def _ensure_started(self):
@@ -26,13 +39,18 @@ class WebCrawler(IWebCrawler):
         await self._ensure_started()
 
         results_markdown = []
-        crawl_results = await self.crawler.arun_many(urls=urls)
+        crawl_results: types.RunManyReturn[str] = await self.crawler.arun_many(
+            urls=urls, config=self.run_config
+        )
 
-        for result in crawl_results:  # type: ignore
+        for result in crawl_results:
             if result.success and result.markdown:
-                results_markdown.append(result.markdown)
+                md_content = (
+                    result.markdown.fit_markdown or result.markdown.raw_markdown
+                )
+                results_markdown.append(md_content)
 
-        return "\n\n".join(results_markdown)
+        return "\n\n---\n\n".join(results_markdown)
 
     async def close(self):
         if self._started:
