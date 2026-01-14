1c2ab7e463e197db844907e939c01534fb3fe549
blagoySimandov <baggy.simandoff@gmail.com>
2026-01-14 22:08:42 +0200

    fix: race condition for  cost tracker

--- go/internal/services/cost_tracker.go
+++ go/internal/services/cost_tracker.go
@@ -1,5 +1,7 @@
 package services
 
+import "sync"
+
 type ICostTracker interface {
 	AddTokenCost(tknIn, tknOut int)
 	AddSearchQueryCost(searchQueryCost int)
@@ -7,8 +9,9 @@ type ICostTracker interface {
 	CostCredits() int
 }
 
-// All prices are in 1/10 cents
+// All prices are 1/10 of a cent
 type CostTracker struct {
+	mu              sync.RWMutex
 	cost            int
 	tknInCost       int
 	tknOutCost      int
@@ -26,18 +29,26 @@ func NewCostTracker(tknInCost, tknOutCost int, searchQueryCost int, creditExchan
 }
 
 func (c *CostTracker) AddTokenCost(tknIn, tknOut int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	totalCost := tknIn*c.tknInCost + tknOut*c.tknOutCost
 	c.cost += totalCost
 }
 
 func (c *CostTracker) AddSearchQueryCost(searchQueryCost int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.searchQueryCost += searchQueryCost
 }
 
 func (c *CostTracker) CostDollars() int {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 	return c.cost
 }
 
 func (c *CostTracker) CostCredits() int {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 	return c.cost * c.creditExchange
 }
